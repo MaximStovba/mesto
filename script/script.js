d9fb31fbdabdeffa5fdc5e7e85bfec4b65f30df9
Maxim Stovba <maxim.stovba@gmail.com>
2020-04-14 22:54:57 +0300

    внесены исправления по итогам 1 ревью

--- script/script.js
+++ script/script.js
@@ -1,43 +1,38 @@
 const content = document.querySelector('.content');
-let popUp = content.querySelector('.popup');
-let editButton = content.querySelector('.profile__edit-button');
-let closeButton = content.querySelector('.popup__icon-close');
-let saveButton = content.querySelector('.popup__btn_action_add');
-let profileTitle = content.querySelector('.profile__title');
-let profileSubtitle = content.querySelector('.profile__subtitle');
-let popupTextTypeName = content.querySelector('.popup__text_type_name');
-let popupTextTypeAbout = content.querySelector('.popup__text_type_about');
-// Находим форму в DOM
-let formElement = content.querySelector('.popup__container');
+const popUp = content.querySelector('.popup'); // Находим pop-up
+const editButton = content.querySelector('.profile__edit-button'); // Находим кнопку редактирования
+const closeButton = content.querySelector('.popup__icon-close'); // Находим кнопку закрытия
+const saveButton = content.querySelector('.popup__btn_action_add'); // Находим кнопку сохранения
+const formElement = content.querySelector('.popup__container'); // Находим форму
+let profileTitle = content.querySelector('.profile__title'); // Находим заголовок "Имени"
+let profileSubtitle = content.querySelector('.profile__subtitle'); // Находим заголовок "О себе"
+let popupTextTypeName = content.querySelector('.popup__text_type_name'); // Находим поле ввода "Имени"
+let popupTextTypeAbout = content.querySelector('.popup__text_type_about'); // Находим поле ввода "О себе"
+let profileAvatar = content.querySelector('.profile__avatar'); // Находим аватар профиля
 
-function openePopup() {
-  popUp.classList.remove('popup_status_closed');
-  popUp.classList.add('popup_status_opened');
-  popupTextTypeName.value = profileTitle.textContent;
-  popupTextTypeAbout.value = profileSubtitle.textContent;
+function popupOpeneClose() {
+  if (popUp.classList.contains('popup_status_closed')) {
+    // действия при открытии
+    popUp.classList.remove('popup_status_closed');
+    popUp.classList.add('popup_status_opened');
+    popupTextTypeName.value = profileTitle.textContent;
+    popupTextTypeAbout.value = profileSubtitle.textContent;
+  } else {
+    // действия при закрытии
+    popUp.classList.remove('popup_status_opened');
+    popUp.classList.add('popup_status_closed');
+  }
 }
 
-function closePopup() {
-  popUp.classList.remove('popup_status_opened');
-  popUp.classList.add('popup_status_closed');
+function formSubmitHandler (evt) { // Обработчик «отправки» формы
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы
+  profileTitle.textContent = popupTextTypeName.value; // Сохраняем значение Имя
+  profileSubtitle.textContent = popupTextTypeAbout.value; // Сохраняем значение О себе
+  profileAvatar.setAttribute('alt', popupTextTypeName.value); // Изменяем "альт" аватара профиля
+  popupOpeneClose(); // Закрываем форму
 }
 
-function savePopup() {
-  popUp.classList.remove('popup_status_opened');
-  popUp.classList.add('popup_status_closed');
-  profileTitle.textContent = popupTextTypeName.value;
-  profileSubtitle.textContent = popupTextTypeAbout.value;
-}
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-}
-
-editButton.addEventListener('click', openePopup);
-closeButton.addEventListener('click', closePopup);
-saveButton.addEventListener('click', savePopup);
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+editButton.addEventListener('click', popupOpeneClose); // слушатель открытия формы
+closeButton.addEventListener('click', popupOpeneClose); // слушатель закрытия формы
+// Прикрепляем обработчик к форме, следит за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
