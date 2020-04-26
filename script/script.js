b8f1d19b1f10d0bce102c1c242c7d0fb665335ae
Maxim Stovba <maxim.stovba@gmail.com>
2020-04-26 20:24:40 +0300

    добавил попап изображения

--- script/script.js
+++ script/script.js
@@ -1,12 +1,17 @@
 const content = document.querySelector('.content');
 const popUpEdit = content.querySelector('.popup_type_edit'); // Находим pop-up редактирования профиля
 const popUpAdd = content.querySelector('.popup_type_add'); // Находим pop-up добавления карточки
+const popUpImg = content.querySelector('.popup_type_image'); // Находим pop-up открытия картинки
+
 const editButton = content.querySelector('.profile__edit-button'); // Находим кнопку редактирования профиля
 const addButton = content.querySelector('.profile__add-button'); // Находим кнопку добавления карточки
 const closeEditFormButton = content.querySelector('.popup__btn-close_formtype_edit'); // Находим кнопку закрытия попапа редактирования профиля
 const closeAddFormButton = content.querySelector('.popup__btn-close_formtype_add'); // Находим кнопку закрытия попапа добавления карточки
+const closeImgFormButton = content.querySelector('.popup__btn-close_formtype_image'); // Находим кнопку закрытия попапа открытия картинки
+
 const formEditElement = content.querySelector('.popup__container_formtype_edit'); // Находим форму редактирования профиля
 const formAddElement = content.querySelector('.popup__container_formtype_add'); // Находим форму добавления карточки
+
 let profileTitle = content.querySelector('.profile__title'); // Находим заголовок "Имени"
 let profileSubtitle = content.querySelector('.profile__subtitle'); // Находим заголовок "О себе"
 let profileAvatar = content.querySelector('.profile__avatar'); // Находим аватар профиля
@@ -14,8 +19,12 @@ let popupTextTypeName = content.querySelector('.popup__text_type_name'); // На
 let popupTextTypeAbout = content.querySelector('.popup__text_type_about'); // Находим поле ввода "О себе"
 let popupTextTypePlace = content.querySelector('.popup__text_type_place'); // Находим поле ввода "Название места"
 let popupTextTypeUrl = content.querySelector('.popup__text_type_url'); // Находим поле ввода "Ссылка на картинку"
+let popupBigImage = content.querySelector('.popup__big-image'); // Находим большое изображение
+let popupFigcaption = content.querySelector('.popup__figcaption'); // Находим подпись большого изображения
+
 const cardTemplate = document.querySelector('#card').content; // Находим шаблон "карточки"
 const cardsContainer = document.querySelector('.elements'); // Элемент куда будем вставлять "карточки"
+
 const initialCards = [
   {
       name: 'Архыз',
@@ -53,14 +62,22 @@ function addNewCard(item) {
   // настройка переключения лайка
   cardElement.querySelector('.elements__like').addEventListener('click', function (evt) {
     evt.target.classList.toggle('elements__like_active');
-});
+  });
   // настройка удаления карточки
   cardElement.querySelector('.elements__trash').addEventListener('click', function (evt) {
     evt.target.parentElement.remove();
-});
+  });
+  // настройка открытия попапа с большым изображением
+  cardElement.querySelector('.elements__image').addEventListener('click', function (evt) {
+    popupBigImage.src = evt.target.src;
+    popupBigImage.alt = evt.target.alt;
+    popupFigcaption.textContent = evt.target.alt;
+    popupOpeneClose('imgButton'); // открываем попап с большим изображением
+  });
   // отображаем на странице в начале блока
   cardsContainer.prepend(cardElement);
 }
+
 // наполняем страницу карточками из массива
 initialCards.forEach(function (card) {
   addNewCard(card);
@@ -69,28 +86,18 @@ initialCards.forEach(function (card) {
 function popupOpeneClose(typeOfButton) {
   switch (typeOfButton) {
     case 'editButton':
-      if (popUpEdit.classList.contains('popup_status_profile-closed')) {
-        // действия при открытии
-        popUpEdit.classList.remove('popup_status_profile-closed');
-        popUpEdit.classList.add('popup_status_profile-opened');
-        popupTextTypeName.value = profileTitle.textContent;
-        popupTextTypeAbout.value = profileSubtitle.textContent;
-      } else if (popUpEdit.classList.contains('popup_status_profile-opened')) {
-        // действия при закрытии
-        popUpEdit.classList.remove('popup_status_profile-opened');
-        popUpEdit.classList.add('popup_status_profile-closed');
-      }
+      popUpEdit.classList.remove('popup_status_profile-closed');
+      popUpEdit.classList.add('popup_status_profile-opened');
+      popupTextTypeName.value = profileTitle.textContent;
+      popupTextTypeAbout.value = profileSubtitle.textContent;
       break;
     case 'addButton':
-      if (popUpAdd.classList.contains('popup_status_item-closed')) {
-        // действия при открытии
-        popUpAdd.classList.remove('popup_status_item-closed');
-        popUpAdd.classList.add('popup_status_item-opened');
-      } else if (popUpAdd.classList.contains('popup_status_item-opened')) {
-        // действия при закрытии
-        popUpAdd.classList.remove('popup_status_item-opened');
-        popUpAdd.classList.add('popup_status_item-closed');
-      }
+      popUpAdd.classList.remove('popup_status_item-closed');
+      popUpAdd.classList.add('popup_status_item-opened');
+      break;
+    case 'imgButton':
+      popUpImg.classList.remove('popup_status_image-closed');
+      popUpImg.classList.add('popup_status_image-opened');
       break;
     case 'closeEditFormButton':
       popUpEdit.classList.remove('popup_status_profile-opened');
@@ -99,6 +106,12 @@ function popupOpeneClose(typeOfButton) {
     case 'closeAddFormButton':
       popUpAdd.classList.remove('popup_status_item-opened');
       popUpAdd.classList.add('popup_status_item-closed');
+      break;
+    case 'closeImgFormButton':
+      popUpImg.classList.remove('popup_status_image-opened');
+      popUpImg.classList.add('popup_status_image-closed');
+      popupTextTypePlace.value = '';
+      popupTextTypeUrl.value = '';
   }
 }
 
@@ -121,26 +134,27 @@ function formAddSubmitHandler (evt) { // Обработчик «отправки
 
 // слушатель открытия формы редактирования профиля
 editButton.addEventListener('click', function (evt) {
-  console.log('Кликнули по элементу editButton');
   popupOpeneClose('editButton');
 });
 
 // слушатель открытия формы добавления карточки
 addButton.addEventListener('click', function (evt) {
-  console.log('Кликнули по элементу addButton');
   popupOpeneClose('addButton');
 });
 
 // слушатель закрытия формы редактирования профиля
 closeEditFormButton.addEventListener('click', function (evt) {
-  console.log('Кликнули по элементу closeEditFormButton');
   popupOpeneClose('closeEditFormButton');
 });
 
 // слушатель закрытия формы добавления карточки
 closeAddFormButton.addEventListener('click', function (evt) {
-  console.log('Кликнули по элементу closeAddFormButton');
   popupOpeneClose('closeAddFormButton');
+});
+
+// слушатель закрытия формы с большим изображением
+closeImgFormButton.addEventListener('click', function (evt) {
+  popupOpeneClose('closeImgFormButton');
 });
 
 // Прикрепляем обработчик к форме редактирования профиля
