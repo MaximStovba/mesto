7e6926696b15198b1208f8ea5d92b26e57ebe200
Maxim Stovba <maxim.stovba@gmail.com>
2020-06-15 23:12:30 +0300

    webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { PopupWithForm } from '../components/PopupWithForm.js';
 import { Section } from '../components/Section.js';
 import { Card } from '../components/Card.js';
 import { FormValidator } from '../components/FormValidator.js';
+import './index.css';
 
 import {
   popUpEdit,
